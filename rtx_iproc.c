1e685811485e1f3c0fea0d9857151eaddeb6a7d2
J2daL <j2dal.jacob@gmail.com>
2010-11-24 22:48:30 -0500

    iProcess Update

--- rtx_iproc.c
+++ rtx_iproc.c
@@ -1,26 +1,31 @@
-interrupt_handler();
+#include "rtx.h"
+
+int signal;
+NewPCB *current_process;
+
+interrupt_handler(signal)
 {
-	save current_process context;
-	current_process->status = blocked_on_interrupt;
+	// save current_process context
+	current_process->state = 2;
 	select interrupt source;
-	switch(signal);
+	switch(signal)
 
 	{
-	A: i_process_timer;
+	case A: // i_process_timer;
 	current_process = i_process_timer;
 	i_process_timer state is set as executing;
 	restore i_process_timer context;
 	invoke i_process_timer handler;
 	break;
 
-	B: i_process_crt;
+	case B: // i_process_crt;
 	current_process = i_process_crt;
 	i_process_crt state is set as executing;
 	restore i_process_crt context;
 	invoke i_process_crt handler;
 	break;
 
-	C: i_process_kb;
+	case C: // i_process_kb
 	current_process = i_process_kb;
 	i_process_kb state is set as executing;
 	restore i_process_kb context;
@@ -28,7 +33,7 @@ interrupt_handler();
 	break;
 	}
 
-	D: i_process_terminate;
+	case D: i_process_terminate;
 save context of interrupt handler
 current_process = save_PCB
 save_PCB = NULL
@@ -36,7 +41,7 @@ set state of current_process to executing
 restore current_process context
 }
 
-i_process_timer:
+i_process_timer
 	{
 		msg_env *timeout_request
 		int temp_sender_pid

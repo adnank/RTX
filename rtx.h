d7d2216397af3dfdc5698a3245d24a44c5651fa5
adnank <adnan.ak@live.ca>
2010-11-26 17:12:33 -0500

    updated rtx.h

--- rtx.h
+++ rtx.h
@@ -1,4 +1,9 @@
 /*will include all structs, globally defined variables, function definitions*/
+#include <string.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #ifndef RTX_H_
 #define RTX_H_
 
@@ -15,7 +20,7 @@
 #define BLK_ON_ENV	12
 #define BLK_ON_RSC	13
 #define INTERRUPT	14
-#define RUNNING		15
+#define EXECUTING	15
 #define IPROCESS	16
 
 //Process ID's
@@ -67,44 +72,75 @@ proc_record init_table[NUM_OF_PROC];	//static array(initialization table)
 
 //****MESSAGE ENVELOPE****
 typedef struct Envelope{
-	struct Envelope *kernelpt; 	//used for creating the list of envelopes
-	struct Envelope *Next; 		// used to put the queue in different lists
-	int SenderID; 			// who sent the message
-	int DestinationID; 		//where the message will be sent
-	int clockticks; 		// for waking,sleeping and ualarm functions
-	int Msg_Type[50];		//type of message
-	char Data [MSG_DATA]; //pointer to an array of characters
+	struct Envelope *kernelpt; //used for creating the list of envelopes
+	struct Envelope *Next; // used to put the queue in different lists
+	struct Envelope *Previous; //used to point to the previous message envelope in the list
+	int SenderID; // who sent the message
+	int DestinationID; //where the message will be sent
+	int clockticks; // for waking,sleeping and ualarm functions
+	int Msg_Type; //type of message
+	char *Data [MSG_DATA]; //pointer to an array of characters
 }Envelope;
 
 //****PROCESS CONTROL BLOCK****
-typedef struct NEWPCB {
-	struct NEWPcb * Kernel_ptr;
-	int State; 					//for the state of the process
-	int ProcID; 				//process id of the process
-	struct NewPcb *Next; 				//pointer to put the PCB in the lists it is supposed to be in
-	int Priority; 				//priority of the process
-	struct NEWPCB *kernelpt; 			//allows kernel to keep a list of all the processes
-	Envelope *head; 			//head for the list of the envelopes that the process owns
-	Envelope *tail; 			//tail for the list of the envelopes that the process owns //FOR WHAT ????
-	Envelope *recievelist; 		//list of envelopes recieved
-	int jbContext; 				//used by setjump and longjump, not sure if returntype=int. ?????????????
+typedef struct NewPCB {
+	struct NewPCB * Kernel_ptr;
+	int State; 							//for the state of the process
+	int ProcID; 						//process id of the process
+	struct NewPCB *Kernelpt_Next; 		//used for making the Process list
+	struct NewPCB *Kernelpt_Previous; 	//used for making the Process List
+	struct NewPCB *Next;				//pointer to put the PCB in the lists it is supposed to be in
+	struct NewPCB *Previous; 			// pointer that points to the previous PCB in the list
+	int Priority; 						//priority of the process
+	struct QueueEnv *Own; 				//list of envelopes that a process owns
+	Envelope *head; 					//head for the list of the envelopes that the process owns
+	Envelope *tail; 					//tail for the list of the envelopes that the process owns //FOR WHAT ????
+	struct QueueEnv *recievelist; 		//list of envelopes recieved
+	char * Stack;
+	int StartAdd;
+	jmp_buf jbContext; 					//used by setjump and longjump, not sure if returntype=int. ?????????????
 }NewPCB;
+
 
 //****MESSAGE ENVELOPE QUEUE****
 typedef struct msg_env_Q{
-	Evelope *head;
+	Envelope *head;
 	Envelope *tail;
 }msg_env_Q;
 
 //****PROCESS QUEUE****
-typedef struct Process_Q{
-	NewPCB *Head;
-	NewPCB *Tail;
-}Proc_Q;
+typedef struct QueuePCB{
+	NewPCB *Head; 						//pointer to the first PCB in the list
+	NewPCB *Tail; 						//pointer to the last PCB in the list
+}QueuePCB;
 
+QueuePCB *ReadyQueue [4];
+QueuePCB *Blocked_On_Resources [4]; 	//lack of recieved messages
+QueuePCB *Blocked_On_Envelope [4]; 		//(free envleopes)pointer that points to the head of the blocked on Envelope queue
+QueuePCB *Blocked_On_interupt [4]; 		//pointer that points to the head of the blocked on interrupt queue
+
+QueueEnv *Free_Env_Queue; 				//pointing to the head and tail of the envelopes in the free envleope queue
+QueuePCB* PCBList; 						//used to make the list of the processes, this pointer points to the first process in the list
+
+//****TRACE BUFFER****
+typedef struct TraceArray{
+	int SenderID;
+	int DestintionID;
+	int Msg_Type;
+}TraceArray;
+
+char * Status_Array [250]; 				//index represents number of processes
+TraceArray* Send_Trace_Array [16];
+int Send_Trace_Array_Counter; 			// counter to keep track how much array is filled
+TraceArray* Recieve_Trace_Array [16];
+int Recieve_Trace_Array_Counter; 		// counter to keep track how much array is filled
+
+NewPCB *current_process; 				//pointer that points to the PCB of the currently executing process
+NewPCB* Executing;						//pointer that points to the pcb of the currently executing process
 
 
 //timeout queue
+
 
 
 
